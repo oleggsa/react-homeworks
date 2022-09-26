0fc9c865cc674bee7e79a1cf504ce930ee2e0c13
Oleh Didyk <braun.ufo@gmail.com>
2022-09-26 16:56:44 +0300

    hw 19

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
-import Card from "./components/Card";
-import Definitions from "./components/Definitions";
+// import Card from "./components/Card";
+// import Definitions from "./components/Definitions";
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function App() {
-  const definitions = [
-    { dt: 'one', dd: 'two', id: 1 },
-    { dt: 'another term', dd: 'another description', id: 2 },
-  ];
+  // const definitions = [
+  //   { dt: 'one', dd: 'two', id: 1 },
+  //   { dt: 'another term', dd: 'another description', id: 2 },
+  // ];
 
   return (
     <>
-      <Card title='Title texta a body absent' />
-      <Card text="text body a title absent"/>
-      <Card title='Title texta' text="text body a vishe title"/>
-      <Definitions data={definitions}/>
+      {/*<Card title='Title texta a body absent' />*/}
+      {/*<Card text="text body a title absent"/>*/}
+      {/*<Card title='Title texta' text="text body a vishe title"/>*/}
+      {/*<Definitions data={definitions}/>*/}
+      <ProgressBar now={30} />
     </>
   );
 }
---
+++ src/components/Progress.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Progress = () => {
+  return (
+    <div>
+      <div className="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"
+           aria-label="progressbar">
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
